a7fd4b9568e379ac190d21bcf127b6c5273b6da7
seimon-m <simimueller@hotmail.com>
2020-11-21 16:59:21 +0100

    add xcode project

---
+++ Albedo/Albedo/AlbedoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbedoApp.swift
+//  Albedo
+//
+//  Created by Simon Müller on 21.11.20.
+//
+
+import SwiftUI
+
+@main
+struct AlbedoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
